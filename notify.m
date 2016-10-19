3c6d64cee4a76928940c4bb67be02e5a13106b94
Daniel Villarreal <danie.villarreal12@gmail.com>
2016-10-19 17:46:50 -0600

    Function to notify of end of execution.

---
+++ notify.m
@@ -0,0 +1,5 @@
+function notify()
+    webread('https://maker.ifttt.com/trigger/spyder/with/key/ou3_qyIHH3IV22kLJrvLT_AJLLNR8PyUqu8dmZvIlGH');
+    system('osascript -e ''display notification "Done in Spyder" sound name "Sound Name"''')
+end
+
