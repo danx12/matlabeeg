d220af8c357593d4bc399b678ff7fb359687ed98
Daniel Villarreal <danielvillarreal@hdmb3.local>
2016-10-30 22:19:02 -0600

    New path centered with Senior as the working directory

--- ReadAllFiles.m
+++ ReadAllFiles.m
@@ -1,5 +1,5 @@
 clc; clear all;
-path2 = '/Volumes/DANFIT1/HifoCap/epilepsiae_patientdata/data/raw_data/pat_1328803/adm_132880103/*/**/*.data';
+path2 = './surf30/pat_22602/adm_226102/*/**/*.data';
 files = rdir(path2);
 files = {files.name};
 
@@ -7,7 +7,7 @@ files = {files.name};
 master_window = [];
 for i=1:length(files)
     disp(['Process ' int2str(i) ' of ' int2str(length(files))]);
-    window = open_study_function(files{i},['T4';'C4';'F7'],3);
+    window = open_study_function(files{i},['CZ';'F3';'C3'],3);
     master_window = vertcat(master_window,window);
 end
 
--- notify.m
+++ notify.m
@@ -1,5 +1,11 @@
-function notify()
-    webread('https://maker.ifttt.com/trigger/spyder/with/key/ou3_qyIHH3IV22kLJrvLT_AJLLNR8PyUqu8dmZvIlGH');
+function notify(mobile)
+    if(isempty(mobile))
+        mobile = 0;
+    end
+    if(mobile)
+        webread('https://maker.ifttt.com/trigger/spyder/with/key/ou3_qyIHH3IV22kLJrvLT_AJLLNR8PyUqu8dmZvIlGH');
+    end
+    
     system('osascript -e ''display notification "Done in Spyder" sound name "Sound Name"''')
 end
 
