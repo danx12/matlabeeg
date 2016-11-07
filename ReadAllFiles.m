e2bee0861ce1dd1e8e3818d3e0d2324bfe650471
Daniel Villarreal <danie.villarreal12@gmail.com>
2016-11-07 14:22:44 -0700

    ReadAllFiles updated for channel separation.

--- ReadAllFiles.m
+++ ReadAllFiles.m
@@ -4,13 +4,23 @@ files = {files.name};
 
 
 %%
-% master_window = [];
+master_window_ch0 = [];
+master_window_ch1 = [];
+master_window_ch2 = [];
+
 for i=1:length(files)
     disp(['Process ' int2str(i) ' of ' int2str(length(files))]);
-%     window = open_study_function(files{i},['CZ';'F3';'C3'],3);
-    eval(['dataInFile_' int2str(i) '= open_study_function(files{i},[''CZ'';''F3'';''C3''],1);']);
-%     master_window = vertcat(master_window,window);
+    window0 = open_study_function(files{i},['CZ'],3);
+    window1 = open_study_function(files{i},['F3'],3);
+    window2 = open_study_function(files{i},['C3'],3);
+    
+%      eval(['dataInFile_' int2str(i) '= open_study_function(files{i},[''CZ'';''F3'';''C3''],1);']);
 end
+     
+ master_window_ch0 = vertcat(master_window_ch0,window0);
+ master_window_ch1 = vertcat(master_window_ch1,window1);
+ master_window_ch2 = vertcat(master_window_ch2,window2);
+
 
 %%
 
