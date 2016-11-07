5f0350eaf4c5be2a2d64b3747843c6c42482d56d
Daniel Villarreal <danielvillarreal@hdmb3.local>
2016-11-07 13:45:43 -0700

    Updated files to handle pat_2206 data

--- ReadAllFiles.m
+++ ReadAllFiles.m
@@ -7,8 +7,8 @@ files = {files.name};
 % master_window = [];
 for i=1:length(files)
     disp(['Process ' int2str(i) ' of ' int2str(length(files))]);
-    window = open_study_function(files{i},['CZ';'F3';'C3'],3);
-    eval(['dataInFile_' int2str(i) '= open_study_function(files{i},[''CZ'';''F3'';''C3''],3);']);
+%     window = open_study_function(files{i},['CZ';'F3';'C3'],3);
+    eval(['dataInFile_' int2str(i) '= open_study_function(files{i},[''CZ'';''F3'';''C3''],1);']);
 %     master_window = vertcat(master_window,window);
 end
 
